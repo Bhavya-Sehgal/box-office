3952d133a6fac3f0dab3cc5bd5f3273cbfdb4cd1
bhavya-sehgal <bhavyasehgal05@gmail.com>
2021-08-18 01:24:09 +0530

    styled show page

--- src/components/show/Seasons.js
+++ src/components/show/Seasons.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { SeasonList, SeasonsWrapper } from './Seasons.styled';
 
 const Seasons = ({ seasons }) => (
-  <div>
+  <SeasonsWrapper>
     <p>
       Seasons in total: <span>{seasons.length}</span>
     </p>
@@ -11,16 +12,16 @@ const Seasons = ({ seasons }) => (
         {seasons.reduce((acc, season) => acc + season.episodeOrder, 0)}
       </span>
     </p>
-    <div>
+    <SeasonList>
       {seasons.map(season => (
-        <div key={season.id}>
-          <div>
+        <div key={season.id} className="season-item">
+          <div className="left">
             <p>Season {season.number}</p>
             <p>
               Episodes: <span>{season.episodeOrder}</span>
             </p>
           </div>
-          <div>
+          <div className="right">
             Aired:{' '}
             <span>
               {season.premiereDate} - {season.endDate}
@@ -28,8 +29,8 @@ const Seasons = ({ seasons }) => (
           </div>
         </div>
       ))}
-    </div>
-  </div>
+    </SeasonList>
+  </SeasonsWrapper>
 );
 
 export default Seasons;
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,6 +6,7 @@ import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const initialState = {
   show: null,
@@ -75,7 +76,7 @@ const Show = () => {
   }
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -84,25 +85,25 @@ const Show = () => {
         tags={show.genres}
       />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
           status={show.status}
           network={show.network}
           premiered={show.premiered}
         />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
