d7fb140efd7a09b6ce8c7e067b6949503ef9ddb3
bhavya-sehgal <bhavyasehgal05@gmail.com>
2021-08-17 15:58:39 +0530

    added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,93 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-console */
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
-const Home = () => <MainPageLayout>This is Home page</MainPageLayout>;
+const Home = () => {
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOptions, setSearchOptions] = useState('shows');
+
+  const isShowsSearch = searchOptions === 'shows';
+
+  const onSearch = () => {
+    apiGet(`/search/${searchOptions}?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const onRadioChange = ev => {
+    setSearchOptions(ev.target.value);
+  };
+
+  console.log(searchOptions);
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
+    }
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
+};
 
 export default Home;
